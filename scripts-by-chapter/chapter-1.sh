90bc54badedfa44b43794beb274efc9fee2722f7
dejan2609 <dejan2609@gmail.com>
2024-12-28 19:15:25 +0100

    some paths are changed

--- scripts-by-chapter/chapter-1.sh
+++ scripts-by-chapter/chapter-1.sh
@@ -4,7 +4,7 @@ echo "***************************************************"
 echo "--- This could take around 20 minutes"
 
 # Create the Kubernetes Cluster
-    eksctl create cluster -f Infrastructure/eksctl/01-initial-cluster/cluster.yaml
+    eksctl create cluster -f ../Infrastructure/eksctl/01-initial-cluster/cluster.yaml
 
 echo "*************************************************************"
 echo "********* READY TO CHAPTER 2! - FINISHED AT $(date) *********"
